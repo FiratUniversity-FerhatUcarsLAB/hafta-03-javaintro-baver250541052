2c0eb8b27a187b4c089c0d66337060600996dc02
baver250541052 <250541052@firat.edu.tr>
2025-10-21 17:14:12 +0300

    Add BesKereTekrar class to print welcome message

---
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -0,0 +1,10 @@
+public class BesKereTekrar {
+
+    public static void main(String[] args) {
+       System.out.println("Java'ya hoş geldiniz!");
+       System.out.println("Java'ya hoş geldiniz!");
+       System.out.println("Java'ya hoş geldiniz!");
+         System.out.println("Java'ya hoş geldiniz!");
+         System.out.println("Java'ya hoş geldiniz!");
+    }
+}
