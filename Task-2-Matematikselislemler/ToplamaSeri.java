a29b9627fce5437e93b40acf9e6d1cdccd88313b
baver250541052 <250541052@firat.edu.tr>
2025-10-21 17:55:43 +0300

    Update ToplamaSeri.java

---
+++ Task-2-Matematikselislemler/ToplamaSeri.java
@@ -0,0 +1,5 @@
+public class ToplamaSerisi {
+    public static void main(String[] args) throws Exception {
+        int toplam=1+2+3+4+5+6+7+8+9;
+        System.out.println("1'den 9'a kadar olan sayıların toplamı: "+toplam);
+}}
