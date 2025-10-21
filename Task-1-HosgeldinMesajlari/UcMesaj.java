796b007ed132bf41b155121a9e9615451f63a42f
baver250541052 <250541052@firat.edu.tr>
2025-10-21 17:06:28 +0300

    Update UcMesaj.java

---
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Java'ya hoş geldiniz!");
+        System.out.println("Merhaba Dünya!");
+        System.out.println("Bilgisayar Bilimlerine Hoş Geldiniz!");
+        System.out.println("Programlama Eğlencelidir!");
+    }
+}
