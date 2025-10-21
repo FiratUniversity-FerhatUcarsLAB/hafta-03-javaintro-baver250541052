90d328282b61453c8c10d0b217aeb335a5476f47
baver250541052 <250541052@firat.edu.tr>
2025-10-21 18:10:00 +0300

    Add IkiIslem class for mathematical operations

---
+++ Task-2-Matematikselislemler/IkiIslem.java
@@ -0,0 +1,17 @@
+public class Ikilslem {
+    public static void main (String[] args)  {
+        // Pay Hesaplama
+        double pay=(9.5*4.5)-(2.5*3);
+
+        // Payda Hesaplama
+        double payda=45.5-3.5;
+        
+        //Bölme Yapma
+        double sonuc=pay/payda;
+
+        // Sonucu Yazdırma
+        System.out.printf("%.4f\n", sonuc);
+    }
+
+
+}
