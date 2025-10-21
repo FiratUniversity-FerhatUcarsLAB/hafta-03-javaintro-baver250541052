2428352691e10c01a5257231a13e76c454daf725
baver250541052 <250541052@firat.edu.tr>
2025-10-21 18:13:12 +0300

    Update Faktoriyel.java

---
+++ Task-2-Matematikselislemler/Faktoriyel.java
@@ -0,0 +1,13 @@
+public class Faktoriyel {
+
+    public static void main(String[] args) {
+        int sayi = 5;
+        int faktoriyel = 1;
+
+        for (int i = 1; i <= sayi; i++) {
+            faktoriyel *= i;
+        }
+
+        System.out.println(sayi + " sayısının faktöriyeli: " + faktoriyel);
+    }
+}
