d53338a14882ac234527982ac3d8ecfee12ccc9b
baver250541052 <250541052@firat.edu.tr>
2025-10-23 21:49:26 +0300

    Add İsimKarti class for name card display

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,12 @@
+public class İsimKarti {
+    public static void main(String[] args) {
+        System.out.println("************************");
+        System.out.println("*      İSİM KARTI     *");
+        System.out.println("************************");
+        System.out.println("* İsim: Ahmet  *");
+        System.out.println("* Soyisim: Yılmaz *");
+        System.out.println("* no: 220301010   *");
+        System.out.println("* Bölüm: Yazılım Mühendisliği  *");
+    
+}
+}
