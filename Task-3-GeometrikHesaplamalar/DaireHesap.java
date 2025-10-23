73c1aa11110b8ec4903293e33b509b428a0c1a3e
baver250541052 <250541052@firat.edu.tr>
2025-10-23 21:30:11 +0300

    Add DairelHesap class for circle calculations

---
+++ Task-3-GeometrikHesaplamalar/DaireHesap.java
@@ -0,0 +1,22 @@
+public class DairelHesap {
+    public static void main(String[] args) {
+        double r = 5.5;
+        double pi = 3.14159;
+
+
+        //  Alan Hesapla
+
+        double alan = pi * r * r ;
+
+        // Cevre Hesapla
+        double cevre = 2 * pi * r ;
+
+        // Sonuclari Yazdir
+        System.out.println("Dairenin Alani: " + alan);
+        System.out.println("Dairenin Cevresi: " + cevre);
+    }
+
+
+
+    
+}
